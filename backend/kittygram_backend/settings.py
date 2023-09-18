d90e045b98fc7269f943044f826aa06b5ab453c5
GodAlexK <Zepp97@yandex.ru>
2023-09-18 09:47:09 +0000

    First version

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['51.250.16.6', '127.0.0.1', 'localhost', 'kittygrambest.ddns.net']
 
